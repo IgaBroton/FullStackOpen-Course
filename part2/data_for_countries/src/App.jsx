4d79d5444a31c388e4cb23833e1922464690b258
Iga <igabroton@gmail.com>
2023-11-09 22:48:21 +0100

    Exercise 2.18

---
+++ part2/data_for_countries/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react'
+import countryService from './services/country'
+import Search from './components/Search';
+
+
+const App = () => {
+
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    countryService
+      .getAll()
+      .then(countries => {
+        setCountries(countries)
+      })
+  }, [])
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+  }
+
+  return (
+    <div>
+      <Search handleSearch={handleSearch}/>
+      
+      {
+      (countries.filter(country => country.name.common.toUpperCase().includes(search.toUpperCase()))).length === 1 ?
+
+      countries.filter(country => country.name.common.toUpperCase().includes(search.toUpperCase()))
+      .map(country => 
+        <div key={country.name.common}> 
+          <h1>{country.name.common}</h1>
+          <div>capital: {country.capital.map(cap => <span key={cap}>{cap} </span>)}</div>
+          <div>area: {country.area}</div>
+          <br />
+          <b>languages:</b>
+          <ul>
+            {Object.entries(country.languages).map(language => <li key={language[0]}>{language[1]}</li>)}
+          </ul>
+          <img src={country.flags.png} height="15%" width="15%" />
+        </div>)
+      :
+      (
+        (countries.filter(country => country.name.common.toUpperCase().includes(search.toUpperCase()))).length > 10 && search != '' ? 
+          'Too many matches, specify another filter' 
+        : 
+        countries.filter(country => country.name.common.toUpperCase().includes(search.toUpperCase()))
+        .map(country => <div key={country.name.common}> {country.name.common} </div>)
+      )
+    }
+    </div>
+  )
+}
+
+export default App
---
+++ part2/data_for_countries/src/components/Search.jsx
@@ -0,0 +1,7 @@
+const Search = ({handleSearch}) => {
+    return (
+        <p>find countries <input onChange={handleSearch} /></p>
+    )
+}
+
+export default Search
