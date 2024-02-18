00de4221bc87cbd37ba2e58fca16e3cbc0ae675d
IgaBroton <igabroton@gmail.com>
2024-02-18 16:55:02 +0100

    Exercise 3.5

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,57 +1,71 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+
+const app = express();
+const port = 3001;
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
 
+app.use(express.json());
 
-app.get('/info', (request, response) => {
-  response.send('<p>Phonebook has info for ' + persons.length + " people</p>" + Date())
-})
+app.get('/info', (request, response) =>
+  response.send(
+    `The phonebook contains records of ${
+      persons.length
+    } people.<br>${new Date()}`
+  )
+);
 
-app.get('/api/persons',(request, response) => {
-  response.json(persons)
-})
+const generateId = () => {
+  const id = Math.ceil(Math.random() * 1000000);
+  if (persons.some((person) => person.id === id)) return generateId();
+  return id;
+};
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons.push(person);
+  response.json(person);
+});
+
+app.get('/api/persons', (request, response) => response.json(persons));
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if(person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
-})
+  const person = persons.find((person) => person.id === +request.params.id);
+  if (!person) return response.status(404).end();
+  response.json(person);
+});
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(note => person.id !== id)
+  persons = persons.filter((person) => person.id !== +request.params.id);
+  response.status(204).end();
+});
 
-  response.status(204).end()
-})
-
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+app.listen(port, () => console.log(`Server listening on port ${port}.`));
